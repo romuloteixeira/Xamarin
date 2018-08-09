567297f87cb18d10e195d54d9b29bfa8a3e81082
romuloteixeira <romulo_teixeira@hotmail.com>
2018-08-08 21:49:56 -0300

    Create App to make a call using a numbers and characters. | Criando App para realizar chamadas usando letras e números.

---
+++ Phoneword/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+	{
+        string translateNumber;
+
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+            translateNumber = PhonewordTranslator.ToNumber(phoneNumberText);
+
+            if (!string.IsNullOrWhiteSpace(translateNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translateNumber;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
